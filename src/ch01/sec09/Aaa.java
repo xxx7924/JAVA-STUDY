011e64f934ae4c7c88cf653539ad95b492037eee
xxx7924 <xxx7924@gmail.com>
2024-05-31 23:47:54 +0900

    1~4 로그 추가

--- src/ch01/sec09/Aaa.java
+++ src/ch01/sec09/Aaa.java
@@ -5,6 +5,11 @@ public class Aaa {
 	public static void main(String[] args) {
 		
 		System.out.println("Aaa");
+		
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
+		System.out.println("444");
 
 
 	}
