549f7a8f79b2c163134ec340a5a4b41b0c916647
xxx7924 <xxx7924@gmail.com>
2026-01-12 21:55:59 +0900

    츄가

--- src/ch01/sec09/Baa.java
+++ src/ch01/sec09/Baa.java
@@ -5,10 +5,6 @@ public class Baa {
 	public static void main(String[] args) {
 		
 		System.out.println("Baa");
-		
-		System.out.println("1111");
-		System.out.println("2222");
-		System.out.println("3333");
 
 
 	}
