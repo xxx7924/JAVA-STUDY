8ac3fc97ec3e6de47d6b00334a64134205062164
xxx7924 <xxx7924@gmail.com>
2026-01-12 21:02:52 +0900

    222

--- src/ch01/sec09/Aaa.java
+++ src/ch01/sec09/Aaa.java
@@ -5,7 +5,7 @@ public class Aaa {
 	public static void main(String[] args) {
 		
 		System.out.println("Aaa");
-		
+		System.out.println("000");
 		System.out.println("111");
 		System.out.println("222");
 		System.out.println("333");
