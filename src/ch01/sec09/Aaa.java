2e4ac70900db9e7cef0a6bbad81c9f22bf6efffc
xxx7924 <160731237+xxx7924@users.noreply.github.com>
2026-01-12 22:00:10 +0900

    Update Aaa.java

--- src/ch01/sec09/Aaa.java
+++ src/ch01/sec09/Aaa.java
@@ -5,14 +5,6 @@ public class Aaa {
 	public static void main(String[] args) {
 		
 		System.out.println("Aaa");
-		System.out.println("000");
-		System.out.println("111");
-		System.out.println("222");
-		System.out.println("333");
-		System.out.println("444");
-		
-		System.out.println("aaaaa");
-		System.out.println("bbbbb");
 
 
 	}
